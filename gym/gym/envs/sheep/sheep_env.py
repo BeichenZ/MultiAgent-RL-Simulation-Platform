3f1350e4d1a33591aa9cd6629a6413662b30b5f9
Zara <annne.lim@hotmail.ca>
2017-10-14 18:46:57 -0700

    makes sheeps appear on a canvas at specified coordinates

--- gym/gym/envs/sheep/sheep_env.py
+++ gym/gym/envs/sheep/sheep_env.py
@@ -1,15 +1,51 @@
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
+from gym.envs.sheep import rendering
 
 class SheepEnv(gym.Env):
   metadata = {'render.modes': ['human']}
 
   def __init__(self):
-    ...
+    self.action_space = spaces.Discrete(4)
+    self.viewer = None
+    self.sheep_positions = [[50, 100], [267, 234]]
+    self.reset()
+
   def _step(self, action):
-    ...
+  	# use Boids to update sheep positions. Below is just a placeholder
+  	# which make them move diagonally
+  	for i in range(len(self.sheep_positions)):
+  		self.sheep_positions[i][0] += 1
+  		self.sheep_positions[i][1] += 1
+  	return
+
   def _reset(self):
-    ...
+  	return
+
   def _render(self, mode='human', close=False):
-    ...
+    if close:
+        if self.viewer is not None:
+            self.viewer.close()
+            self.viewer = None
+        return
+    screen_width = 600
+    screen_height = 400
+
+    if self.viewer is None:
+        self.viewer = rendering.Viewer(screen_width, screen_height)
+        self.circTranlations = []
+        for i in range(len(self.sheep_positions)):
+            translation = rendering.Transform()
+            circ = rendering.make_circle(20)
+            circ.set_color(.5, .5, 0.5)
+            circ.add_attr(translation)
+            self.viewer.add_geom(circ)
+            self.circTranlations.append(translation)
+        
+
+    for ind, translation in enumerate(self.circTranlations):
+    	translation.set_translation(self.sheep_positions[ind][0], self.sheep_positions[ind][1])
+    
+    return self.viewer.render(return_rgb_array = mode=='rgb_array')
+    
